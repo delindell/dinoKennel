3cb506e6011cd69b75d65e6e493202a37eeaaea1
delindell <daviselindell@gmail.com>
2020-02-22 10:21:16 -0600

    reset form complete

--- main.js
+++ main.js
@@ -9,6 +9,14 @@ const printDinos = () => {
 
 const dinos = [];
 
+// const clearNewDinoForm = () => {
+//     document.getElementById('dino-name').value = '';
+//     document.getElementById('dino-type').value = '';
+//     document.getElementById('dino-age').value = '';
+//     document.getElementById('dino-owner').value = '';
+//     document.getElementById('dino-image').value = '';
+// };
+
 const newDino = (e) => {
     e.preventDefault()
     const brandNewDino = {
@@ -22,6 +30,7 @@ const newDino = (e) => {
         imageUrl: document.getElementById('dino-image').value
     }
     dinos.push(brandNewDino);
+    document.getElementById('new-dino-form').reset(); 
     console.log('hello', dinos);
 };
 
