d50c240f57a26d6ef81be2ac0fe751d0a2207892
delindell <daviselindell@gmail.com>
2020-02-22 10:14:40 -0600

    created new dino object

--- main.js
+++ main.js
@@ -1,96 +1,3 @@
-const dinos = [
-    {
-      id: 'dino1',
-      name: 'Rex',
-      type: 'T Rex',
-      age: 100,
-      owner: 'Zoe',
-      adventures: [],
-      health: 100,
-      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-    },
-    {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-      {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-      {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-      {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-      {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-      {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-      {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-      {
-        id: 'dino1',
-        name: 'Rex',
-        type: 'T Rex',
-        age: 100,
-        owner: 'Zoe',
-        adventures: [],
-        health: 100,
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-      },
-  ]
-
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
@@ -100,29 +7,27 @@ const printDinos = () => {
 
 }
 
+const dinos = [];
+
 const newDino = (e) => {
     e.preventDefault()
-    console.log('click')
-    dinoName = document.getElementById('dino-name').value;
-    console.log(dinoName)
-    dinos.name.push(dinoName);
-    console.log(dinos)
-    dinoAge = document.getElementById('dino-age').value;
-    dinos.age.push(dinoAge)
-    document.getElementsByClassName('card-body').value = '';
-    console.log(dinos);
-}
+    const brandNewDino = {
+        id: `dinos${dinos.length +1}`,
+        name: document.getElementById('dino-name').value,
+        type: document.getElementById('dino-type').value,
+        age: document.getElementById('dino-age').value,
+        owner: document.getElementById('dino-owner').value,
+        adventures: [],
+        health: 100,
+        imageUrl: document.getElementById('dino-image').value
+    }
+    dinos.push(brandNewDino);
+    console.log('hello', dinos);
+};
 
-
-
-
-const event = () => {
-    document.getElementById('create-dino-btn').addEventListener('click', newDino);
-}
-
-const init = (function (){
-    event();
-});
+const init = () => {
+    document.getElementById('submit-new-dino').addEventListener('click', newDino);
+};
 
 init();
 
